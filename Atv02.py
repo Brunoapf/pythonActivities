5e1dc52481989679e27dafb3bb8941840a706b39
Bruno <brunoaraujopf@gmail.com>
2026-03-11 08:11:50 -0300

    Att

--- Atv02.py
+++ Atv02.py
@@ -7,3 +7,17 @@ s = input("Digite sua senha: ")
 if u == usuario and s == senha:
     print("Login bem-sucedido! Bem-vindo, " + usuario + "!")
 else:    print("Login falhou! Usuário ou senha incorretos.")
+
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor >= segundo_valor:
+    print(
+        f'{primeiro_valor=} é maior ou igual '
+        f'ao que {segundo_valor=}'
+    )
+else:
+    print(
+        f'{segundo_valor=} é maior '
+        f'do que {primeiro_valor=}'
+    )
